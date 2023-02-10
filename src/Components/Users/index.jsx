70fd6e7418f5ed7624cec7a70b355ae03d144469
AlaaRaed1 <102379774+AlaaRaed1@users.noreply.github.com>
2023-02-10 18:41:48 +0300

    added pagination

--- src/Components/Users/index.jsx
+++ src/Components/Users/index.jsx
@@ -7,8 +7,10 @@ import {
   TableBody,
   TableRow,
   TableCell,
+  TablePagination,
   Paper,
 } from "@mui/material";
+
 const UsersTable = () => {
   const [listOfUsers, setListOfUsers] = useState([]);
   const getListOfUsers = async () => {
@@ -19,32 +21,55 @@ const UsersTable = () => {
     getListOfUsers();
   }, []);
 
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+  const emptyRows =
+    rowsPerPage -
+    Math.min(rowsPerPage, listOfUsers.length - page * rowsPerPage);
   return (
     <TableContainer component={Paper}>
-      <Table aria-label="Users Table">
+      <Table aria-label="simple table">
         <TableHead>
           <TableRow>
             <TableCell>Id</TableCell>
-            <TableCell>First Name</TableCell>
-
+            <TableCell>Name</TableCell>
             <TableCell>Email</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {listOfUsers.map((row) => {
-            return (
-              <TableRow
-                key={row.id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
+          {listOfUsers
+            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            .map((row, i) => (
+              <TableRow key={i}>
                 <TableCell>{row.id}</TableCell>
                 <TableCell>{row.name}</TableCell>
                 <TableCell>{row.email}</TableCell>
               </TableRow>
-            );
-          })}
+            ))}
+          {emptyRows > 0 && (
+            <TableRow style={{ height: 53 * emptyRows }}>
+              <TableCell colSpan={6} />
+            </TableRow>
+          )}
         </TableBody>
       </Table>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={listOfUsers.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
     </TableContainer>
   );
 };
